ea74d828fe423e95fa6d4e92337e61f25d3846f7
EldersJavas <eldersjavas@gmail.com>
2022-01-23 12:14:34 +0800

    add ci & readme & flag

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"flag"
 	"github.com/ratel-online/core/log"
 	"github.com/ratel-online/core/util/async"
 	"github.com/ratel-online/server/network"
+	"strconv"
+)
+
+var (
+	Wsport  int
+	Tcpport int
 )
 
 func main() {
+	flag.IntVar(&Wsport, "w", 9998, "WebsocketServer Port")
+	flag.IntVar(&Tcpport, "t", 9999, "TcpServer Port")
+
 	async.Async(func() {
-		wsServer := network.NewWebsocketServer(":9998")
+		wsServer := network.NewWebsocketServer(":" + strconv.Itoa(Wsport))
 		log.Panic(wsServer.Serve())
 	})
 
-	server := network.NewTcpServer(":9999")
+	server := network.NewTcpServer(":" + strconv.Itoa(Tcpport))
 	log.Panic(server.Serve())
 }
