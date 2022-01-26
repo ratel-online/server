aed6f2d934320bacef0148ac590d10b674a68658
Nico <ainililia@163.com>
2022-01-26 14:45:42 +0800

    update url.

--- state/welcome.go
+++ state/welcome.go
@@ -11,7 +11,7 @@ type welcome struct{}
 
 func (*welcome) Next(player *database.Player) (consts.StateID, error) {
 	buf := bytes.Buffer{}
-	buf.WriteString(fmt.Sprintf("Hi %s, Welcome to ratel online! rules at https://github.com/ratel-online/discuss/blob/main/RULES.md\n", player.Name))
+	buf.WriteString(fmt.Sprintf("Hi %s, Welcome to ratel online! rules at https://github.com/ratel-online/server/blob/main/README.md\n", player.Name))
 	err := player.WriteString(buf.String())
 	if err != nil {
 		return 0, player.WriteError(err)
