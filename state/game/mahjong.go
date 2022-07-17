d217e3de3edd44fabe95889a9225aba5a014fbb4
len <867869344@qq.com>
2022-07-17 18:17:14 +0800

    update

--- state/game/mahjong.go
+++ state/game/mahjong.go
@@ -56,7 +56,6 @@ func (g *Mahjong) Next(player *database.Player) (consts.StateID, error) {
 
 func (g *Mahjong) Exit(player *database.Player) consts.StateID {
 	room := database.GetRoom(player.RoomID)
-	room.Game = nil
 	room.State = consts.RoomStateWaiting
 	game := room.Mahjong
 	for _, playerId := range game.Players {
