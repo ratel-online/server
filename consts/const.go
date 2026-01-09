f3aa72c3e0195a2b7939f402ee81778efc38c6f5
mikodream <138103099+mikodream@users.noreply.github.com>
2026-01-09 22:52:42 +0800

    feat: 房间 ip 显示设置（隐私处理只显示前两位）防熊 (#52)
    * feat: 房间 ip 显示设置（隐私处理只显示前两位）防熊
    
    * perf: 更正创建模式时的显示顺序，更换麻将和 UNO 的序号
    
    ---------
    
    Co-authored-by: nico <ainililia@163.com>
    Co-authored-by: miko <1059665047@qq.com>

--- consts/const.go
+++ consts/const.go
@@ -54,8 +54,8 @@ const (
 	GameTypeSkill   = 3
 	GameTypeRunFast = 4
 	GameTypeTexas   = 5
-	GameTypeUno     = 6
-	GameTypeMahjong = 7
+	GameTypeMahjong = 6
+	GameTypeUno     = 7
 
 	RobTimeout         = 20 * time.Second
 	PlayTimeout        = 40 * time.Second
@@ -71,6 +71,7 @@ const (
 	RoomPropsPassword   = "pwd"
 	RoomPropsPlayerNum  = "pn"
 	RoomPropsChat       = "ct"
+	RoomPropsShowIP     = "ip"
 )
 
 var MnemonicSorted = []int{15, 14, 2, 1, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3}
@@ -119,11 +120,12 @@ var (
 		GameTypeLaiZi:   "斗地主-癞子版",
 		GameTypeSkill:   "斗地主-大招版",
 		GameTypeRunFast: "跑得快",
+		GameTypeTexas:   "德州扑克",
 		//GameTypeUno:     "Uno",
 		GameTypeMahjong: "Mahjong",
-		GameTypeTexas:   "德州扑克",
+		
 	}
-	GameTypesIds = []int{GameTypeClassic, GameTypeLaiZi, GameTypeSkill, GameTypeRunFast, GameTypeMahjong, GameTypeTexas}
+	GameTypesIds = []int{GameTypeClassic, GameTypeLaiZi, GameTypeSkill, GameTypeRunFast, GameTypeTexas,GameTypeMahjong}
 	RoomStates   = map[int]string{
 		RoomStateWaiting: "Waiting",
 		RoomStateRunning: "Running",
